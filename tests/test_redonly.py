2d75f5f311913e63ff8a85bb7fe8e2e598202016
Cyril NOVEL <5690282+cnovel@users.noreply.github.com>
2023-08-01 16:36:14 +0200

    Support for multilanguage

--- tests/test_redonly.py
+++ tests/test_redonly.py
@@ -14,10 +14,14 @@ class TestRedOnly():
         shutil.rmtree(self.temp_folder)
         print(f"Folder {self.temp_folder} removed!")
     
-    def test_two_subs(self):
-        redonly = ro.RedOnly(self.temp_folder, ["france", "amitheasshole"])
-        assert redonly.generate(), "Failed to generate RedOnly!"
+    def test_two_french_subs(self):
+        redonly = ro.RedOnly(self.temp_folder, ["france", "Lyon"], ro.Language.fr)
+        assert redonly.generate(), "Failed to generate RedOnly in French!"
     
+    def test_two_en_subs(self):
+        redonly = ro.RedOnly(self.temp_folder, ["CasualUK", "news"], ro.Language.en)
+        assert redonly.generate(), "Failed to generate RedOnly in English!"
+
     def test_wrong_sub(self):
         redonly = ro.RedOnly(self.temp_folder, ["cesousnexistepasmongars"])
         assert not redonly.generate(), "Generation should not be successful for a non existent subreddit"
